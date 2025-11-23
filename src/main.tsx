7639c123f840e3bd89f57932fdc0a9d143605625
치훈 <rlaclgns321@naver.com>
2025-11-23 15:47:41 +0900

    chore: 절대경로, 자동완성 환경 세팅완료

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import './index.css'
 import App from './App'
+import { worker } from '@/mocks/browser'
 
 async function enableMocking() {
   if (!import.meta.env.DEV) {
     // true MSW on, false MSW off
     return
   }
-  const { worker } = await import('./mocks/browser') // 이전에 설정한 브라우저 환경설정 import
 
   return worker.start({
     onUnhandledRequest: 'bypass', // 모킹되지 않은 요청은 실제 서버로 전달
