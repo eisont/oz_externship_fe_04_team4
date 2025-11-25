c4fd6a1296d6e7241dfc31578cc12e9e79a8a707
chodayoung <verysweetkey@gmail.com>
2025-11-25 11:31:42 +0900

    feat: 공통 컴포넌트 버튼 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,23 @@
 import AppRouter from '@/app/router'
+import Button from '@/components/common/Button'
 import './App.css'
 
 export default function App() {
-  return <AppRouter />
+  return (
+    <>
+      <AppRouter />
+      <Button variant="close">닫기</Button>
+      <Button variant="delete">삭제하기</Button>
+      <Button variant="cancel">취소</Button>
+      <Button variant="custom" className="bg-primary-green text-white">
+        복구
+      </Button>
+      <Button variant="custom" className="bg-primary-blue text-white">
+        수정하기
+      </Button>
+      <Button variant="custom" className="bg-primary-yellow text-white">
+        적용하기
+      </Button>
+    </>
+  )
 }
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,36 @@
-export function Button() {
-  return <div>Button</div>
+import clsx from 'clsx'
+import type { ButtonHTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type ButtonVariant = 'close' | 'cancel' | 'delete' | 'custom'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant
+  children: ReactNode
+  className?: string
+}
+
+const variantStyles: Record<ButtonVariant, string> = {
+  close: `btn btn-filled`,
+  cancel: `btn border-1 border-[#D1D5DB]`,
+  delete: `btn btn-delete`,
+  custom: `btn`,
+}
+
+export default function Button({
+  variant = 'custom',
+  className,
+  children,
+  ...props
+}: ButtonProps) {
+  const baseStyle = `
+    box-border btn py-2 px-4 text-[16px]
+  `
+  const merged = twMerge(clsx(baseStyle, variantStyles[variant], className))
+
+  return (
+    <button className={merged} {...props}>
+      {children}
+    </button>
+  )
 }
