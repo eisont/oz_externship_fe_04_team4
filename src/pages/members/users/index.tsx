8084307f690f1b5c889157a01df8f259f1b051ea
chodayoung <verysweetkey@gmail.com>
2025-12-01 16:33:01 +0900

    feat:유저 관리 index 추가

--- src/pages/members/users/index.tsx
+++ src/pages/members/users/index.tsx
@@ -1,5 +1,7 @@
 // 회원관리 / 유저관리
 
+import UserTable from '@/pages/members/users/UserTable'
+
 export default function AdminUserManagementPage() {
-  return <>회원관리 / 유저관리</>
+  return <UserTable />
 }
