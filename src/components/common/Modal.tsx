bff20acbd5c3bd808194a8edc5cf586c92ecc0b7
chodayoung <verysweetkey@gmail.com>
2025-12-02 10:44:39 +0900

    fix: 모달 컴포넌트 body 스크롤 방지 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -33,12 +33,30 @@ export default function Modal({
   const modalRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
     if (isOpen) {
+      const scrollY = window.scrollY
+
+      document.body.style.position = 'fixed'
+      document.body.style.top = `-${scrollY}px`
+      document.body.style.left = '0'
+      document.body.style.right = '0'
+      document.body.style.overflow = 'hidden'
+      document.body.style.width = '100%'
+
       const prevActiveElement = document.activeElement as HTMLElement
 
       requestAnimationFrame(() => {
         modalRef.current?.focus()
       })
       return () => {
+        document.body.style.position = ''
+        document.body.style.top = ''
+        document.body.style.left = ''
+        document.body.style.right = ''
+        document.body.style.overflow = ''
+        document.body.style.width = ''
+
+        window.scrollTo(0, scrollY)
+
         prevActiveElement?.focus()
       }
     }
