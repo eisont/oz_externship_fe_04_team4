32543979145c1c7257087b02d696fa0752bf0ca7
치훈 <rlaclgns321@naver.com>
2025-12-10 16:26:36 +0900

    refactor: main Provider를 App으로, 페이지 레이아웃을 app/layout으로 이전

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,19 @@
-import AppRouter from '@/app/routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router'
+
+import AppRouter from '@/app/router'
 import './App.css'
 
 export default function App() {
-  return <AppRouter />
+  const queryClient = new QueryClient()
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <StrictMode>
+          <AppRouter />
+        </StrictMode>
+      </QueryClientProvider>
+    </BrowserRouter>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 
 import App from '@/app/App'
 import { worker } from '@/mocks/browser'
@@ -12,20 +9,9 @@ async function enableMocking() {
 
   if (import.meta.env.VITE_USE_MOCK !== 'true') return
 
-  return worker.start({
-    onUnhandledRequest: 'bypass', // 모킹되지 않은 요청은 실제 서버로 전달
-  })
+  return worker.start({ onUnhandledRequest: 'bypass' })
 }
-const queryClient = new QueryClient()
 
 enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </QueryClientProvider>
-    </BrowserRouter>
-  )
+  createRoot(document.getElementById('root')!).render(<App />)
 })
